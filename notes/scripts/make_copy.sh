c64bae4c414dca7784a084c22bcd520ef615452c
Amir Sadeghi <54184877+amirhs1@users.noreply.github.com>
2026-04-17 13:24:27 -0400

    Remove notes folder from project

--- notes/scripts/make_copy.sh
+++
@@ -1,7 +0,0 @@
-for file in */*; do
-    if [ -f "$file" ]; then
-        # Replace the slash with an underscore for the new filename
-        newname=$(echo "$file" | sed 's/\//_/g')
-        cp "$file" "../$newname"
-    fi
-done
